536ac6049629ca235e0f5a5be0f5cc370e89894b
Tim Fadeev <1mailforwork1@gmail.com>
2024-12-01 12:10:24 +0300

    feat(repository): add `CrudRepository` interface

---
+++ src/main/java/org/as1iva/repository/CrudRepository.java
@@ -0,0 +1,14 @@
+package org.as1iva.repository;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudRepository<K extends Serializable, E> {
+
+    E save(E entity);
+
+    Optional<E> findById(K id);
+
+    List<E> findAll();
+}
