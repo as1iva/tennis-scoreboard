626dea97ed4d8952a056dc43d4f3e83167c4000d
Tim Fadeev <1mailforwork1@gmail.com>
2024-12-09 16:56:37 +0300

    feat(repository): add `findByName` method in `PlayerRepository`

--- src/main/java/org/as1iva/repository/PlayerRepository.java
+++ src/main/java/org/as1iva/repository/PlayerRepository.java
@@ -1,10 +1,24 @@
 package org.as1iva.repository;
 
 import org.as1iva.entity.Player;
+import org.hibernate.Session;
+
+import java.util.Optional;
 
 public class PlayerRepository extends BaseRepository<Long, Player> {
 
     public PlayerRepository() {
         super(Player.class);
+    }
+
+    public Optional<Player> findByName(String name) {
+        try(Session session = sessionFactory.openSession()) {
+
+            return Optional.ofNullable(
+                    session.createQuery("FROM Player WHERE name = :name", Player.class)
+                            .setParameter("name", name)
+                            .uniqueResult()
+            );
+        }
     }
 }
