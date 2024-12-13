b8ba17ce5d7afc54829f2530db180b34db350e5e
Tim Fadeev <1mailforwork1@gmail.com>
2024-12-13 16:52:19 +0300

    feat(servlet): add `doGet` method in `MatchScoreServlet`

---
+++ src/main/java/org/as1iva/servlet/MatchScoreServlet.java
@@ -0,0 +1,30 @@
+package org.as1iva.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.as1iva.dto.MatchScoreDto;
+import org.as1iva.service.OutgoingMatchesService;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@WebServlet("/match-score")
+public class MatchScoreServlet extends HttpServlet {
+
+    private final OutgoingMatchesService outgoingMatchesService = new OutgoingMatchesService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
+
+        MatchScoreDto matchScoreDto = outgoingMatchesService.getMatchScore(uuid);
+
+        req.setAttribute("matchScoreDto", matchScoreDto);
+        req.setAttribute("uuid", uuid);
+
+        req.getRequestDispatcher("/jsp/match-score.jsp").forward(req, resp);
+    }
+}
