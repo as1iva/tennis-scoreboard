4e45f512c6a5dacc76dedf267ae358226c15c5e9
Tim Fadeev <1mailforwork1@gmail.com>
2024-12-22 16:47:11 +0300

    feat(servlet): add rendering finished matches logic in `MatchesServlet`

--- src/main/java/org/as1iva/servlet/MatchesServlet.java
+++ src/main/java/org/as1iva/servlet/MatchesServlet.java
@@ -5,14 +5,22 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.as1iva.dto.MatchResponseDto;
+import org.as1iva.service.FinishedMatchesPersistenceService;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/matches")
 public class MatchesServlet extends HttpServlet {
 
+    private final FinishedMatchesPersistenceService finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<MatchResponseDto> matches = finishedMatchesPersistenceService.getAllMatches();
+
+        req.setAttribute("matches", matches);
         req.getRequestDispatcher("/jsp/matches.jsp").forward(req, resp);
     }
 }
