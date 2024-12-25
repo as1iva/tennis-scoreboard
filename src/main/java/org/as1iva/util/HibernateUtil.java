db7991f1e4e740120751b30566d24736bfda332a
Tim Fadeev <1mailforwork1@gmail.com>
2024-12-25 23:24:06 +0300

    feat(util): add `@UtilityClass` annotation in `HibernateUtil`

--- src/main/java/org/as1iva/util/HibernateUtil.java
+++ src/main/java/org/as1iva/util/HibernateUtil.java
@@ -1,9 +1,11 @@
 package org.as1iva.util;
 
 import lombok.Getter;
+import lombok.experimental.UtilityClass;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+@UtilityClass
 public class HibernateUtil {
 
     @Getter
